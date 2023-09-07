fedca91e165dddac906b31be08f2d3166726c26c
rishavmngo <rishavinmngo@gmail.com>
2023-09-07 08:51:21 +0530

    added utils.go

--- utils/utils.go
+++ utils/utils.go
@@ -1,1 +1,17 @@
 package utils
+
+import (
+	"encoding/json"
+	"net/http"
+)
+
+func RespondWithJson(w http.ResponseWriter, code int, payload interface{}) {
+	response, _ := json.Marshal(payload)
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(code)
+	w.Write(response)
+}
+
+func RespondWithError(w http.ResponseWriter, code int, message string) {
+	RespondWithJson(w, code, map[string]string{"error": message})
+}
