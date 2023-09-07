f55ba6869587ab759adff3b59ce3253503c4bda3
rishavmngo <rishavinmngo@gmail.com>
2023-09-07 15:44:52 +0530

    Reimplement Nullablestring to work correctly

--- types/Nullablestring.go
+++ types/Nullablestring.go
@@ -1,36 +1,60 @@
 package types
 
 import (
-	"database/sql"
-	"encoding/json"
+	"database/sql/driver"
+	"errors"
 )
 
-type NullableString sql.NullString
+// "database/sql"
+// "encoding/json"
 
-func (ns *NullableString) Value() sql.NullString {
-	s := ns.String
-	if len(s) == 0 {
-		return sql.NullString{}
-	}
-	return sql.NullString{
-		String: s,
-		Valid:  true,
-	}
-}
+// type NullableString sql.NullString
+//
+// func (ns *NullableString) Value() sql.NullString {
+// 	s := ns.String
+// 	if len(s) == 0 {
+// 		return sql.NullString{}
+// 	}
+// 	return sql.NullString{
+// 		String: s,
+// 		Valid:  true,
+// 	}
+// }
+//
+// func (ns *NullableString) UnmarshalJSON(data []byte) error {
+// 	if string(data) == "null" {
+// 		ns.Valid = false
+// 		ns.String = ""
+// 		return nil
+// 	}
+//
+// 	var s string
+// 	if err := json.Unmarshal(data, &s); err != nil {
+// 		return err
+// 	}
+//
+// 	ns.Valid = true
+// 	ns.String = s
+// 	return nil
+// }
 
-func (ns *NullableString) UnmarshalJSON(data []byte) error {
-	if string(data) == "null" {
-		ns.Valid = false
-		ns.String = ""
+type NullableString string
+
+func (s *NullableString) Scan(value interface{}) error {
+	if value == nil {
+		*s = ""
 		return nil
 	}
-
-	var s string
-	if err := json.Unmarshal(data, &s); err != nil {
-		return err
+	strVal, ok := value.(string)
+	if !ok {
+		return errors.New("Column is not a string")
 	}
-
-	ns.Valid = true
-	ns.String = s
+	*s = NullableString(strVal)
 	return nil
+}
+func (s NullableString) Value() (driver.Value, error) {
+	if len(s) == 0 { // if nil or empty string
+		return nil, nil
+	}
+	return string(s), nil
 }
