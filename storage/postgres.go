5348c34cee74fb725dd320e2b36a5c1ce5a71c81
rishavmngo <rishavinmngo@gmail.com>
2023-09-08 13:32:31 +0530

    added chat message and recipient tables

--- storage/postgres.go
+++ storage/postgres.go
@@ -28,11 +28,11 @@ func InitilizePostgresStore(user, password, dbname, port string) *Postgres {
 var queries []string
 
 func ensureTableExist(db *sql.DB) {
-	queries = []string{userTable}
+	queries = []string{userTable, chatTable, messageTable, recipientsTable, privateChatTable}
 
-	if _, err := db.Exec(userTable); err != nil {
-		log.Fatal(err)
-	}
+	// if _, err := db.Exec(userTable); err != nil {
+	// 	log.Fatal(err)
+	// }
 
 	for _, query := range queries {
 
@@ -52,6 +52,54 @@ const userTable = `CREATE TABLE IF NOT EXISTS users
 	CONSTRAINT user_pkey PRIMARY KEY (id),
 	CONSTRAINT user_username_unique unique (username),
 	CONSTRAINT user_email_unique unique (email)
+)`
+
+const chatTable = `CREATE TABLE IF NOT EXISTS chats
+(
+	id SERIAL,
+	created_at TIMESTAMP NOT NULL,
+	CONSTRAINT chat_pkey PRIMARY KEY (id)
+)`
+
+const messageTable = `CREATE TABLE IF NOT EXISTS messages
+(
+	id SERIAL,
+	chat_id INT ,
+	author_id INT,
+	CONSTRAINT message_pkey PRIMARY KEY (id),
+	CONSTRAINT fk_chat
+		FOREIGN KEY(chat_id)
+			REFERENCES chats(id),
+	CONSTRAINT fk_users
+		FOREIGN KEY(author_id)
+			REFERENCES users(id)
+)`
+
+const recipientsTable = `CREATE TABLE IF NOT EXISTS recipients 
+(
+	id SERIAL,
+	message_id INT ,
+	receiver_id INT,
+	CONSTRAINT recipients_pkey PRIMARY KEY (id),
+	CONSTRAINT fk_chat
+		FOREIGN KEY(message_id)
+			REFERENCES messages(id),
+	CONSTRAINT fk_users
+		FOREIGN KEY(receiver_id)
+			REFERENCES users(id)
+)`
+const privateChatTable = `CREATE TABLE IF NOT EXISTS private_chat
+(
+	id SERIAL,
+	chat_id INT ,
+	member_id INT,
+	CONSTRAINT private_chat_pkey PRIMARY KEY (id),
+	CONSTRAINT fk_chat
+		FOREIGN KEY(chat_id)
+			REFERENCES chats(id),
+	CONSTRAINT fk_users
+		FOREIGN KEY(member_id)
+			REFERENCES users(id)
 )`
 
 func (postgres *Postgres) AddUser(user *types.User) error {
