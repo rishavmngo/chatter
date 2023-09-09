b3ec3b9a1088bc2da040c6560cd6484370ac6fd3
rishavmngo <rishavinmngo@gmail.com>
2023-09-09 20:36:16 +0530

    implement chats controller

--- api/chats/chats.controller.go
+++ api/chats/chats.controller.go
@@ -4,9 +4,12 @@ import (
 	"encoding/json"
 	"log"
 	"net/http"
+	"strconv"
 
+	"github.com/gorilla/mux"
 	"github.com/rishavmngo/chatter-backend/intrf"
 	"github.com/rishavmngo/chatter-backend/types"
+	"github.com/rishavmngo/chatter-backend/utils"
 )
 
 type controller struct {
@@ -15,6 +18,17 @@ type controller struct {
 
 func (controller *controller) GetById(w http.ResponseWriter, r *http.Request) {
 
+	chat_id, _ := strconv.Atoi(mux.Vars(r)["chat_id"])
+	chat := types.Chat{}
+
+	chat.ID = uint(chat_id)
+
+	if err := controller.db.GetChatById(&chat); err != nil {
+		utils.RespondWithError(w, http.StatusInternalServerError, err.Error())
+		return
+	}
+
+	utils.RespondWithJson(w, http.StatusOK, chat)
 }
 
 func (controller *controller) Add(w http.ResponseWriter, r *http.Request) {
@@ -26,4 +40,11 @@ func (controller *controller) Add(w http.ResponseWriter, r *http.Request) {
 	if err := decoder.Decode(&chat); err != nil {
 		log.Fatal(err)
 	}
+
+	if err := controller.db.AddChat(&chat); err != nil {
+		utils.RespondWithError(w, http.StatusInternalServerError, err.Error())
+		return
+	}
+
+	utils.RespondWithJson(w, http.StatusOK, chat)
 }
